e0f85e2ef8d410ba64087aecdd53b88f5c3db80c
jonte265 <200072657+jonte265@users.noreply.github.com>
2026-08-18 22:13:56 +0200

    only solo

--- lib/matches.ts
+++ lib/matches.ts
@@ -1,7 +1,7 @@
 export function getQueueName(queueId: number) {
   const queues: Record<number, string> = {
     400: "Normal Draft",
-    420: "Ranked Solo/Duo",
+    420: "Ranked Solo",
     430: "Normal Blind",
     440: "Ranked Flex",
     450: "ARAM",
