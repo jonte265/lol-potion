f00dbe30e51c9ef718da4e23d5c8b4ab2a7ff112
jonte265 <200072657+jonte265@users.noreply.github.com>
2026-08-19 11:48:12 +0200

    team stats row own component

---
+++ components/features/team-stats-row.tsx
@@ -0,0 +1,137 @@
+import Image from "next/image"
+import { TableCell, TableRow } from "../ui/table"
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip"
+import Typography from "../common/typography/Typography"
+import Link from "next/link"
+import { Badge } from "../ui/badge"
+import { Flame } from "lucide-react"
+import { Progress } from "../ui/progress"
+import { getChampionImageUrl } from "@/lib/ddragon"
+import { calcCs, calcCsPerMin } from "@/lib/stat"
+import { formatCompactNumber } from "@/lib/format"
+
+export default function TeamStatsRow({
+  player,
+  puuid,
+  gameDuration,
+  highestDamage,
+}) {
+  const cs = calcCs(player.totalMinionsKilled, player.neutralMinionsKilled)
+
+  const csPerMin = calcCsPerMin(cs, gameDuration)
+
+  return (
+    <TableRow
+      className={`${player.win ? "bg-primary/30" : "bg-destructive/20"} `}
+    >
+      <TableCell>
+        <div className="flex flex-row items-center gap-2">
+          {/* Champ icon */}
+          <div className="relative shrink-0">
+            <Image
+              className={`${player.puuid === puuid ? "rounded-full ring-1 ring-foreground/80" : "rounded-xs"} `}
+              src={getChampionImageUrl(player.championName)}
+              width={40}
+              height={40}
+              alt={`${player.championName} icon`}
+            />
+
+            <div className="absolute bottom-0 rounded-xs bg-background px-0.5">
+              <Typography small>{player.champLevel}</Typography>
+            </div>
+            <div className="absolute top-0 left-1/2 z-10 -translate-x-1/2 -translate-y-1/2 rounded-xs bg-background px-0.5 py-0">
+              <Tooltip>
+                <TooltipTrigger>
+                  <Typography light small>
+                    {player.summonerLevel}
+                  </Typography>
+                </TooltipTrigger>
+                <TooltipContent>
+                  Summoner level when this match was played
+                </TooltipContent>
+              </Tooltip>
+            </div>
+          </div>
+
+          <div className="flex flex-col">
+            <Link
+              className={`block w-24 truncate text-sm font-bold hover:underline`}
+              href={`/profile/${player.riotIdGameName}/${player.riotIdTagline}`}
+              title={player.riotIdGameName}
+            >
+              {player.riotIdGameName}
+            </Link>
+            <div className="flex flex-row gap-1">
+              <Typography light>{player.championName}</Typography>
+              {player.pentaKills > 0 && <Badge>Pentakill</Badge>}
+            </div>
+          </div>
+        </div>
+      </TableCell>
+      <TableCell>51</TableCell>
+      <TableCell>
+        <div className="flex flex-row gap-1">
+          <Typography>{player.kills}</Typography>
+          <Typography light>/</Typography>
+          <div>
+            <Typography>{player.deaths}</Typography>
+          </div>
+          <Typography light>/</Typography>
+          <Typography>{player.assists}</Typography>
+        </div>
+        <div className="flex flex-row gap-1">
+          <Typography bold>{player.challenges.kda.toFixed(1)}</Typography>
+          <Typography light>KDA</Typography>
+        </div>
+      </TableCell>
+      <TableCell>
+        <div className="flex flex-col gap-1">
+          <div className="flex flex-row items-center gap-1">
+            {player.totalDamageDealtToChampions === highestDamage && (
+              <Flame className="size-4 text-orange-400" />
+            )}
+            <Typography>
+              {new Intl.NumberFormat("en-US").format(
+                player.totalDamageDealtToChampions
+              )}{" "}
+            </Typography>
+          </div>
+          <div>
+            <Progress
+              className={`[&_[data-slot=progress-track]]:h-2 ${
+                player.win
+                  ? "[&_[data-slot=progress-indicator]]:bg-primary"
+                  : "[&_[data-slot=progress-indicator]]:bg-destructive"
+              }`}
+              value={(player.totalDamageDealtToChampions / highestDamage) * 100}
+            />
+          </div>
+        </div>
+      </TableCell>
+      <TableCell>
+        <Typography>{formatCompactNumber(player.goldEarned)}</Typography>
+      </TableCell>
+      <TableCell>
+        <Typography>{cs}</Typography>
+        <Typography light>({csPerMin.toFixed(1)})</Typography>
+      </TableCell>
+      <TableCell>
+        <Tooltip>
+          <TooltipTrigger>
+            <Typography>{player.visionScore}</Typography>
+          </TooltipTrigger>
+          <TooltipContent>
+            <div className="flex flex-col">
+              <Typography>{player.wardsPlaced} wards placed</Typography>
+              <Typography>{player.wardsKilled} wards destroyed</Typography>
+              <Typography>
+                {player.detectorWardsPlaced} Control wards placed
+              </Typography>
+            </div>
+          </TooltipContent>
+        </Tooltip>
+      </TableCell>
+      <TableCell className="text-right">Item1</TableCell>
+    </TableRow>
+  )
+}
--- components/features/team-stats-table.tsx
+++ components/features/team-stats-table.tsx
@@ -16,6 +16,7 @@ import {
 } from "../ui/table"
 import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip"
 import { Badge } from "../ui/badge"
+import TeamStatsRow from "./team-stats-row"
 
 export default function TeamStatsTable({
   team,
@@ -26,7 +27,6 @@ export default function TeamStatsTable({
   return (
     <div className="overflow-hidden rounded-xl">
       <Table>
-        {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
         <TableHeader>
           <TableRow>
             <TableHead className="min-w-32">
@@ -53,143 +53,15 @@ export default function TeamStatsTable({
           </TableRow>
         </TableHeader>
         <TableBody>
-          {team.map((player) => {
-            console.log("player9", player)
-
-            const cs = calcCs(
-              player.totalMinionsKilled,
-              player.neutralMinionsKilled
-            )
-
-            const csPerMin = calcCsPerMin(cs, gameDuration)
-
-            return (
-              <TableRow
-                className={`${player.win ? "bg-primary/30" : "bg-destructive/20"} `}
-                key={player.puuid}
-              >
-                <TableCell>
-                  <div className="flex flex-row items-center gap-2">
-                    {/* Champ icon */}
-                    <div className="relative shrink-0">
-                      <Image
-                        className={`${player.puuid === puuid ? "rounded-full ring-1 ring-foreground/80" : "rounded-xs"} `}
-                        src={getChampionImageUrl(player.championName)}
-                        width={40}
-                        height={40}
-                        alt={`${player.championName} icon`}
-                      />
-
-                      <div className="absolute bottom-0 rounded-xs bg-background px-0.5">
-                        <Typography small>{player.champLevel}</Typography>
-                      </div>
-                      <div className="absolute top-0 left-1/2 z-10 -translate-x-1/2 -translate-y-1/2 rounded-xs bg-background px-0.5 py-0">
-                        <Tooltip>
-                          <TooltipTrigger>
-                            <Typography light small>
-                              {player.summonerLevel}
-                            </Typography>
-                          </TooltipTrigger>
-                          <TooltipContent>
-                            Summoner level when this match was played
-                          </TooltipContent>
-                        </Tooltip>
-                      </div>
-                    </div>
-
-                    <div className="flex flex-col">
-                      <Link
-                        className={`block w-24 truncate text-sm font-bold hover:underline`}
-                        href={`/profile/${player.riotIdGameName}/${player.riotIdTagline}`}
-                        title={player.riotIdGameName}
-                      >
-                        {player.riotIdGameName}
-                      </Link>
-                      <div className="flex flex-row gap-1">
-                        <Typography light>{player.championName}</Typography>
-                        {player.pentaKills > 0 && <Badge>Pentakill</Badge>}
-                      </div>
-                    </div>
-                  </div>
-                </TableCell>
-                <TableCell>51</TableCell>
-                <TableCell>
-                  <div className="flex flex-row gap-1">
-                    <Typography>{player.kills}</Typography>
-                    <Typography light>/</Typography>
-                    <div>
-                      <Typography>{player.deaths}</Typography>
-                    </div>
-                    <Typography light>/</Typography>
-                    <Typography>{player.assists}</Typography>
-                  </div>
-                  <div className="flex flex-row gap-1">
-                    <Typography bold>
-                      {player.challenges.kda.toFixed(1)}
-                    </Typography>
-                    <Typography light>KDA</Typography>
-                  </div>
-                </TableCell>
-                <TableCell>
-                  <div className="flex flex-col gap-1">
-                    <div className="flex flex-row items-center gap-1">
-                      {player.totalDamageDealtToChampions === highestDamage && (
-                        <Flame className="size-4 text-orange-400" />
-                      )}
-                      <Typography>
-                        {new Intl.NumberFormat("en-US").format(
-                          player.totalDamageDealtToChampions
-                        )}{" "}
-                      </Typography>
-                    </div>
-                    <div>
-                      <Progress
-                        className={`[&_[data-slot=progress-track]]:h-2 ${
-                          player.win
-                            ? "[&_[data-slot=progress-indicator]]:bg-primary"
-                            : "[&_[data-slot=progress-indicator]]:bg-destructive"
-                        }`}
-                        value={
-                          (player.totalDamageDealtToChampions / highestDamage) *
-                          100
-                        }
-                      />
-                    </div>
-                  </div>
-                </TableCell>
-                <TableCell>
-                  <Typography>
-                    {formatCompactNumber(player.goldEarned)}
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography>{cs}</Typography>
-                  <Typography light>({csPerMin.toFixed(1)})</Typography>
-                </TableCell>
-                <TableCell>
-                  <Tooltip>
-                    <TooltipTrigger>
-                      <Typography>{player.visionScore}</Typography>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                      <div className="flex flex-col">
-                        <Typography>
-                          {player.wardsPlaced} wards placed
-                        </Typography>
-                        <Typography>
-                          {player.wardsKilled} wards destroyed
-                        </Typography>
-                        <Typography>
-                          {player.detectorWardsPlaced} Control wards placed
-                        </Typography>
-                      </div>
-                    </TooltipContent>
-                  </Tooltip>
-                </TableCell>
-                <TableCell className="text-right">Item1</TableCell>
-              </TableRow>
-            )
-          })}
+          {team.map((player) => (
+            <TeamStatsRow
+              key={player.puuid}
+              player={player}
+              puuid={puuid}
+              gameDuration={gameDuration}
+              highestDamage={highestDamage}
+            />
+          ))}
         </TableBody>
       </Table>
     </div>
